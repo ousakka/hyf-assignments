8944092f60fe62ab472bfc04647a5c397a8bc0a2
Oumaima Sakka <oumaima.sakka.os@gmail.com>
2025-11-06 10:30:13 +0100

    Modify the code based on the mentor feedback

--- courses/foundation/intro-to-javascript/Assignment/script.js
+++ courses/foundation/intro-to-javascript/Assignment/script.js
@@ -1,6 +1,6 @@
 /* Age-ify (A future age calculator) */
-let yearOfBirth=1999;
-let yearFuture=2045;
+const yearOfBirth=1999;
+const yearFuture=2045;
 let age=yearFuture-yearOfBirth;
 console.log("You will be " + age + " years old in " + yearFuture);
 
@@ -19,11 +19,11 @@ if (shouldShowResultInDogYears) {
 }
 
 /* Housey pricey (A house price estimator) */
-let houseHeightPeter = 10;
-let houseWidthPeter = 8;
-let houseDepthPeter = 10;
+const houseHeightPeter = 10;
+const houseWidthPeter = 8;
+const houseDepthPeter = 10;
 let houseVolumePeter = houseHeightPeter * houseWidthPeter * houseDepthPeter;
-let gardenSizeInM2Peter = 100;
+const gardenSizeInM2Peter = 100;
 
 let housePricePeter = houseVolumePeter * 2.5 * 1000 + gardenSizeInM2Peter * 300;
 if (housePricePeter <= 2500000) {
@@ -32,11 +32,11 @@ if (housePricePeter <= 2500000) {
     console.log("Peter is paying too little");
 }
 
-let houseHeightJulia = 8;
-let houseWidthJulia = 5;
-let houseDepthJulia = 11;
+const houseHeightJulia = 8;
+const houseWidthJulia = 5;
+const houseDepthJulia = 11;
 let houseVolumeJulia = houseHeightJulia * houseWidthJulia * houseDepthJulia;
-let gardenSizeInM2Julia = 70;
+const gardenSizeInM2Julia = 70;
 let housePriceJulia = houseVolumeJulia * 2.5 * 1000 + gardenSizeInM2Julia * 300;
 if (housePriceJulia <= 1000000) {
     console.log("Julia is paying too much");
@@ -46,8 +46,8 @@ if (housePriceJulia <= 1000000) {
 
 /* Ez Namey (Startup name generator) */
 
-let firstWords=["Easy","Smart","NextGen","Quick","Green","Tech","Bright","Future","Innovative","Dynamic"];
-let secondWords=["Solutions","Systems","Technologies","Concepts","Designs","Ideas","Ventures","Labs","Works","Studios"];
+const firstWords=["Easy","Smart","NextGen","Quick","Green","Tech","Bright","Future","Innovative","Dynamic"];
+const secondWords=["Solutions","Systems","Technologies","Concepts","Designs","Ideas","Ventures","Labs","Works","Studios"];
 
 let startupName;
 let randomIndex=Math.floor(Math.random()*10);
