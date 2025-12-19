2a52a1b2db4e30fad4014e0948ba8796cd8f96a2
Oumaima Sakka <oumaima.sakka.os@gmail.com>
2025-12-19 10:58:25 +0100

    Modify the SQL code based on the mentor comments

--- courses/foundation/databases/DatabaseAssignment.sql
+++ courses/foundation/databases/DatabaseAssignment.sql
@@ -3,15 +3,24 @@ SELECT Count(*) as total FROM task;
 --2 How many tasks in the task table do not have a valid due date?
 SELECT count(*) as due_date_not_valid from task where due_date is NULL;
 --3 Find all the tasks that are marked as done.
-SELECT * FROM task WHERE status_id=3;
+SELECT t.*
+FROM task t
+JOIN status s ON t.status_id = s.id
+WHERE s.name = 'Done';
 --4 Find all the tasks that are not marked as done.
-SELECT * FROM task WHERE status_id =1 OR status_id =2;
+SELECT t.*
+FROM task t
+JOIN status s ON t.status_id = s.id
+WHERE s.name != 'Done';
 --5 Get all the tasks, sorted with the most recently created first.
 SELECT * FROM task ORDER BY created DESC;
 --6 Get the single most recently created task.
 SELECT * FROM task ORDER BY created DESC LIMIT 1;
 --7 Get the title and due date of all tasks where the title or description contains database.
-select title,due_date,description from task where task.description like '%database%' or task.title like '%database%';
+SELECT title, due_date, description
+FROM task
+WHERE task.description LIKE '%database%'
+   OR task.title LIKE '%database%';
 --8 Get the title and status (as text) of all tasks.
 SELECT title,
 CASE status_id 
@@ -22,6 +31,13 @@ END AS status_text
 FROM task;
 
 --9 Get the name of each status, along with a count of how many tasks have that status.
-select (select name from status where status.id=task.status_id) as status_name,count(*) as total_tasks from task group by status_name; 
+SELECT s.name AS status_name, COUNT(t.id) AS total_tasks
+FROM status s
+LEFT JOIN task t ON t.status_id = s.id
+GROUP BY s.name;
 --10 Get the names of all statuses, sorted by the status with most tasks first.
-select (select name from status where status.id=task.status_id) as status_name,count(*) as total_tasks from task group by status_name order by total_tasks desc;
+SELECT s.name AS status_name, COUNT(t.id) AS total_tasks
+FROM status s
+LEFT JOIN task t ON t.status_id = s.id
+GROUP BY s.name
+ORDER BY total_tasks DESC;
